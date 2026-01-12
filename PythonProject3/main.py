4a6f52bc162eda989bf0d9460da84f4dd2687f28
Eminence-18 <2406531@students.kcau.ac.ke>
2026-01-12 17:56:18 +0300

    Add files via upload

---
+++ PythonProject3/main.py
@@ -0,0 +1,5 @@
+def binary_string_to_int(num_servers, num_players, num_admins):
+    getNumServers = int(num_servers, 2)
+    getNumPlayers = int(num_players, 2)
+    getNumAdmins = int(num_admins, 2)
+    return getNumServers, getNumPlayers, getNumAdmins
