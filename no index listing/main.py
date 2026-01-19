ba5c527bb1cd0a887939762a17409fcb32ce1564
Eminence-18 <2406531@students.kcau.ac.ke>
2026-01-19 12:08:26 +0300

    Add files via upload

---
+++ no index listing/main.py
@@ -0,0 +1,11 @@
+def contains_leather_scraps(items):
+    found = False
+
+    # don't touch above this line
+
+    if "Leather Scraps" in items:
+        found = True
+
+    # don't touch below this line
+
+    return found
