4f1959f116c2437d1442da3f25de95950d685a86
Eminence-18 <2406531@students.kcau.ac.ke>
2026-01-20 19:57:29 +0300

    Add files via upload

---
+++ dictionaries/main.py
@@ -0,0 +1,5 @@
+def get_character_record(name, server, level, rank):
+    id = name + "#" + server
+    return {"name": name, "server": server, "level": level, "rank": rank, "id": id}
+
+ # id = name + "#" + server
