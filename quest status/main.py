e5ee4b8858e71b08ed3d778894d638948d77ae26
Eminence-18 <2406531@students.kcau.ac.ke>
2026-01-27 15:59:46 +0300

    Add files via upload

---
+++ quest status/main.py
@@ -0,0 +1,3 @@
+def get_quest_status(progress):
+    # the next line here displays chaining keys in a dictionary to access a dictionary within another dictionary
+    return progress["quests"]["bridge_run"]["status"]
